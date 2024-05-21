a5e3547820af7f2237e284a1db98aa6c0be33da9
Nicolas Juarez <nicolas.juarez86@gmail.com>
2024-05-21 17:40:08 -0300

    tipografia, home, menu

--- javascript/lib/jquery.scrolled.js
+++ javascript/lib/jquery.scrolled.js
@@ -2,10 +2,10 @@
 /* SCROLLED */
 
 $(document).ready(function(){
-    var HEADER_HEIGHT = 160;
-    var HEADER_HEIGHT_SCROLLED = 90;
+    var HEADER_HEIGHT = 190;
+    var HEADER_HEIGHT_SCROLLED = 100;
   
-    $('.Header-scroll').scrolled({
+    $('.Header').scrolled({
       scroll: HEADER_HEIGHT - HEADER_HEIGHT_SCROLLED
     });
   });
